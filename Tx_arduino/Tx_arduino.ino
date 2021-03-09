bfa0700a5912de4f04afa42c70c95450fb386732
Rudolf Maununen <R.Maununen@student.tudelft.nl>
2021-03-09 20:14:06 +0100

    installed communication with serial

--- Tx_arduino/Tx_arduino.ino
+++ Tx_arduino/Tx_arduino.ino
@@ -1,10 +1,23 @@
 //Transmit from here
+byte inBytes;
 void setup() {
-  // put your setup code here, to run once:
-
+Serial.begin(9600);
+pinMode(13,1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+if (Serial.available()>0){
+  inBytes = Serial.read();
+  if (inBytes == 1){
+    digitalWrite(13,1);
+    Serial.write("1 is sent");
+  }
+  if (inBytes == 0){
+    digitalWrite(13,0);
+    Serial.write("0 is sent");
+  }
+  else{
+    Serial.write("Invalid information");
+  }
+}
 }
--- Tx_data.py
+++ Tx_data.py
@@ -1,1 +1,13 @@
 #Transmit from here
+import serial
+import time
+serialcom = serial.Serial('/dev/cu.usbserial-1410', 9600, 1) #Change port name to the one you are using
+
+While True:
+    i = int(input('input (1 or 0): ').strip())
+    if i == 2:
+        print("finished")
+        break
+    serialcom.write(i.encode())
+    print(serialcom.readline().decode('ascii'))
+serialcom.close()
