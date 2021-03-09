02fe0964b0818b58120e57474e1c713d8abfeaa7
Rudolf Maununen <R.Maununen@student.tudelft.nl>
2021-03-09 19:24:32 +0100

    added ino files

---
+++ Rx_arduino/Rx_arduino.ino
@@ -0,0 +1,10 @@
+//Receive here
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Tx_arduino/Tx_arduino.ino
@@ -0,0 +1,10 @@
+//Transmit from here
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
