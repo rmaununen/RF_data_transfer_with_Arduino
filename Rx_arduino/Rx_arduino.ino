2f9e448045b92826814518641ef311ad566cd4a7
Rudolf Maununen <R.Maununen@student.tudelft.nl>
2021-03-14 17:09:27 +0100

    Made a basis for receiving functionality

--- Rx_arduino/Rx_arduino.ino
+++ Rx_arduino/Rx_arduino.ino
@@ -1,10 +1,43 @@
 //Receive here
+String inBytes = "0";
+bool waiting = true;
+int Voltage;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0,0);
+  pinMode(13,1);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(waiting){
+    digitalWrite(13,1);
+    if (Serial.available()>0){
+      inBytes = Serial.readStringUntil('\n');
+      if (inBytes == "1"){
+        waiting = false;
+        digitalWrite(13,0);
+        Serial.println("START RECEIVING DATA");
+      }
+    }
+    delay(5);
+  }
+  
+  Voltage = analogRead(A0);
+  Serial.println(Voltage);
+  
+  if (Serial.available()>0){
+      inBytes = Serial.readStringUntil('\n');
+      if (inBytes == "0"){
+        Serial.println("STOP RECEIVING DATA\n");
+        waiting = true;
+        digitalWrite(13,0);
+        delay(500);
+      }
+    }
+  
+  delay(50);
+
 
 }
--- Rx_data.py
+++ Rx_data.py
@@ -1,1 +1,38 @@
 #Receive here
+import serial
+import time
+#1410 or 1420
+serialcom = serial.Serial('/dev/cu.usbserial-1420', baudrate=9600, timeout=1) #Change port name to the one you are using
+
+receiving = True
+time.sleep(2)
+received_data = []
+serialcom.write("1\n".encode())
+time_init = time.time()
+print('start time: ', time_init)
+while receiving:
+    bytesToRead = serialcom.inWaiting()
+    data = serialcom.read(bytesToRead).decode('ascii')[:-2]
+    if not data == '':
+        received_data.append(data)
+    time.sleep(0.01) #must be faster than the arduino
+    time_cur = time.time()
+    if (time_cur - time_init) >= 5:
+        receiving = False
+        serialcom.write("0\n".encode())
+        time.sleep(0.4)
+        print('end time: ', time_cur)
+
+bytesToRead = serialcom.inWaiting()
+data = serialcom.read(bytesToRead).decode('ascii')[:-2]
+serialcom.close()
+print(data)
+'''
+received_data.append(data[0])
+received_data.append(data[1])
+print(data[2])
+print(data[3])
+'''
+
+print('\n', received_data)
+print('length of the received array: ', len(received_data))
